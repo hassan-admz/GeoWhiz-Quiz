c9675b6fce66b431f8d3ee59287bbab4f64e801e
hassan-admz <83913603+hassan-admz@users.noreply.github.com>
2024-02-27 07:38:38 +0800

    New Project
    - Setup programmatic UIKit
    - Organised files into appropriate folders

--- GeoWhiz Quiz/Controller/ViewController.swift
+++ GeoWhiz Quiz/Controller/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  GeoWhiz Quiz
 //
-//  Created by user on 27/2/24.
+//  Created by Hassan Mayers on 27/2/24.
 //
 
 import UIKit
@@ -11,9 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemTeal
+        navigationItem.title = "GeoWhiz Quiz"
     }
-
-
 }
 
