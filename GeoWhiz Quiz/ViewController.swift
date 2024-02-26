b0acb18da46ef0af39e405b049e473666ab41a6b
hassan-admz <83913603+hassan-admz@users.noreply.github.com>
2024-02-27 07:22:38 +0800

    Initial Commit

---
+++ GeoWhiz Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeoWhiz Quiz
+//
+//  Created by user on 27/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
